eb12a53dc6f9b37971804b0e9cfa939df7307bbf
juan <juanvillarroelcontacto@gmail.com>
2025-02-06 20:10:29 -0400

    Next.js dashboard practicas

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,3 +1,9 @@
 import  { Inter } from 'next/font/google';
 
-export const inter = Inter({ subsets: ['latin'] });
+export const lusitana = Inter({ subsets: ['latin'], weight: '700' });
+
+export const inter = Inter({ 
+    subsets: ['latin'], 
+    weight: ['400', '700'] });
+
+
